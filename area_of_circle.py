fa24a1ee50117750dd2434da9b91ae803e04d632
AnandSavvy <99013863+AnandSavvy@users.noreply.github.com>
2022-02-08 00:04:04 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,4 @@
+import math as M  
+Radius = float (input ("Please enter the radius of the given circle: "))  
+area_of_the_circle = M.pi* Radius * Radius  
+print (" The area of the given circle is: ", area_of_the_circle)
