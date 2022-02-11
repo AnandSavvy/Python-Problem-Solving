fec59af8274625bed8a0240df16dc6f1875c7ad2
Anand <99013863+AnandSavvy@users.noreply.github.com>
2022-02-12 00:06:07 +0530

    Add files via upload

---
+++ Access the nested key increment from the following dictionary.py
@@ -0,0 +1,12 @@
+emp_dict = {
+    "company": {
+        "employee": {
+            "name": "nandhu",
+            "payable": {
+                "salary": 9000,
+                "increment": 12
+            }
+        }
+    }
+}
+print(emp_dict['company']['employee']['payable']['increment'])
